804d86e63d5517ec5d4b8ec6f38cc176a7fbc33c
Gerardo Mikael Do Carmo Pereira <gerardomikael1234@gmail.com>
2025-03-13 13:33:06 -0300

    Deixar barra de navegação automática funcional

--- global.js
+++ global.js
@@ -18,12 +18,19 @@ currentLink?.classList.add("current");*/
 
 let pages = [
     {url: "", title: "Home"},
-    {url: "projects/", title: "Projects"},
-    {url: "contact/", title: "Contact"},
-    {url: "resume/", title: "Resume"},
+    {url: "/projects/", title: "Projects"},
+    {url: "/contact/", title: "Contact"},
+    {url: "/resume/", title: "Resume"},
     {url: "https://github.com/G-Mikael", title: "github"}
 ];
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // Create link and add it to nav
+    nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
+}
 

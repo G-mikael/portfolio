4f8a2064c504da0b1b7793a4bcbd658f8ebc5135
Gerardo Mikael do Carmo Pereira <B46994@fgv.edu.br>
2025-03-13 12:21:08 -0300

    adicionar barra de navegação automatica com js

--- global.js
+++ global.js
@@ -1,5 +1,29 @@
-console.log("IT’S ALIVE!");
+/*console.log("IT’S ALIVE!");
 
 function $$ (selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+    a => a.host === location.host && a.pathname === location.pathname
+);
+
+if (currentLink) { // or if (currentLink !== undefined)
+    currentLink.classList.add("current");
+}
+
+currentLink?.classList.add("current");*/
+
+let pages = [
+    {url: "", title: "Home"},
+    {url: "projects/", title: "Projects"},
+    {url: "contact/", title: "Contact"},
+    {url: "resume/", title: "Resume"},
+    {url: "https://github.com/G-Mikael", title: "github"}
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
